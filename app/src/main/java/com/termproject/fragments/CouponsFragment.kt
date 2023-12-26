a20b7a5a7a3218ef31582c954f225fe0c6151d40
Can <can.kayikci@ug.bilkent.edu.tr>
2023-12-26 20:32:15 +0300

    Coupon and Profile Page Update

--- app/src/main/java/com/termproject/fragments/CouponsFragment.kt
+++ app/src/main/java/com/termproject/fragments/CouponsFragment.kt
@@ -1,10 +1,20 @@
 package com.termproject.fragments
 
 import android.os.Bundle
+import android.util.Log
+import android.view.GestureDetector
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.GestureDetectorCompat
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.snackbar.Snackbar
+import com.termproject.CouponRecyclerViewAdapter
+import com.termproject.FixturesRecyclerViewAdapter
 import com.termproject.R
 import com.termproject.databinding.FragmentCouponBinding
 import com.termproject.databinding.FragmentCouponsBinding
@@ -17,6 +27,8 @@ import com.termproject.databinding.FragmentCouponsBinding
  */
 class CouponsFragment : Fragment() {
     lateinit var binding: FragmentCouponsBinding
+    private var gDetector: GestureDetectorCompat? = null
+    private lateinit var adapter: CouponRecyclerViewAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,11 +39,55 @@ class CouponsFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentCouponsBinding.inflate(
-            layoutInflater
-        )
+        binding = FragmentCouponsBinding.inflate(layoutInflater)
 
+        val recyclerView = binding.CouponRecyclerView
+        val layoutManager = LinearLayoutManager(context)
+        layoutManager!!.orientation = LinearLayoutManager.VERTICAL
+        recyclerView.layoutManager = layoutManager
+
+        adapter = CouponRecyclerViewAdapter(requireContext())
+
+        setupRecyclerView()
+        gDetector = GestureDetectorCompat(requireContext(), CustomGesture())
+
+        recyclerView.adapter = adapter
         return binding.root
+    }
+    private fun setupRecyclerView() {
+        // ... existing RecyclerView setup ...
+
+        val itemTouchHelperCallback = object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
+            override fun onMove(recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder, target: RecyclerView.ViewHolder): Boolean {
+                return false // not important in this context
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val position = viewHolder.adapterPosition // get position of the swiped item
+
+                Snackbar.make(binding.CouponRecyclerView, "Bet deleted", Snackbar.LENGTH_LONG).show()
+                Log.d("BET", "DELETED")
+            }
+        }
+
+        val itemTouchHelper = ItemTouchHelper(itemTouchHelperCallback)
+        itemTouchHelper.attachToRecyclerView(binding.CouponRecyclerView)
+    }
+
+    inner class CustomGesture: GestureDetector.SimpleOnGestureListener() {
+
+        override fun onLongPress(e: MotionEvent) {
+            Log.i("gesturebro", "onLongPress")
+        }
+
+        override fun onFling(e1: MotionEvent?, e2: MotionEvent, velocityX: Float, velocityY: Float): Boolean {
+            Log.i("gesturebro", "onFling")
+            return false
+        }
+        override fun onDoubleTapEvent(e: MotionEvent): Boolean {
+            Log.i("gesturebro", "onDoubleTapEvent")
+            return true
+        }
     }
 
 
