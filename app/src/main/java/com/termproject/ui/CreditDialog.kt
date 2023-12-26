321435f3e973b21c73ea7729dd53f84736f540cd
Baturalp Sönmez <baturalpsonmez2002@gmail.com>
2023-12-26 20:20:14 +0300

    credit dialog updates

--- app/src/main/java/com/termproject/ui/CreditDialog.kt
+++ app/src/main/java/com/termproject/ui/CreditDialog.kt
@@ -1,11 +1,14 @@
 package com.termproject.ui
 
-import android.app.Dialog
+
 import android.content.Context
-import android.os.Bundle
+import android.util.AttributeSet
+import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
-import com.termproject.databinding.DialogCoinBinding
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.termproject.R
 import com.termproject.db.user.User
 import com.termproject.db.user.UserViewModel
 import kotlinx.coroutines.CoroutineScope
@@ -13,60 +16,62 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class CreditDialog(context: Context, userViewModel: UserViewModel, creditBtn: Button) :
-    Dialog(context) {
+    MaterialAlertDialogBuilder(context) {
     private var userViewModel: UserViewModel
     private var creditBtn: Button
-    lateinit var binding: DialogCoinBinding
-
-    init {
-        this.userViewModel = userViewModel
-        this.creditBtn = creditBtn
-    }
-
 
     private lateinit var existingUser: User
 
-    override fun onAttachedToWindow() {
-        super.onAttachedToWindow()
+    init {
+        this.userViewModel = userViewModel
+
+
 
         CoroutineScope(Dispatchers.Main).launch {
             existingUser = userViewModel.getUser()
 
             if (existingUser != null) {
 
+                setMessage(
+                    existingUser.credit.toString()
+                )
 
-                binding.tvCoinAmount.setText(existingUser.credit.toString())
             }
 
 
         }
 
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        binding = DialogCoinBinding.inflate(layoutInflater)
-
-        setContentView(binding.root)
+        this.creditBtn = creditBtn
+        setTitle("Your Credits:")
 
 
 
 
 
-        binding.btnClose.setOnClickListener {
-            dismiss()
+
+
+        setNegativeButton(
+            "Close"
+        ) { dialog, _ ->
+            dialog.dismiss()
         }
 
-        binding.btnResetCoin.setOnClickListener {
+        setPositiveButton(
+            "Reset Credits"
+        ) { dialog, _ ->
             CoroutineScope(Dispatchers.Main).launch {
                 existingUser.credit = 10000
                 creditBtn.text = "10000"
+
                 userViewModel.updateUser(existingUser)
                 Toast.makeText(context, "Your credit is resetted successfully", Toast.LENGTH_SHORT)
                     .show()
-                dismiss()
+                dialog.dismiss()
             }
         }
+
+
     }
+
+
 }
